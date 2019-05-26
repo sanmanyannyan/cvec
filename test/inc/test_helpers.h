93cc7c5e349119314debc22d52773b9438359b9b
sanmanyannyan <sanmanyannyan@gmail.com>
2019-05-26 02:53:35 +0200

    add test case support random test data

--- test/inc/test_helpers.h
+++ test/inc/test_helpers.h
@@ -16,3 +16,84 @@ do{                                              \
 
 #define STR(t) #t
 #define TEST_FUNCTION(func) test_ ## func
+
+union u32_f32 {
+	float f32;
+	uint32_t u32;
+};
+
+uint32_t read_f32_as_u32(float f) {
+	union u32_f32 uf;
+	uf.f32 = f;
+	return uf.u32;
+}
+
+float read_u32_as_f32(uint32_t u) {
+	union u32_f32 uf;
+	uf.u32 = u;
+	return uf.f32;
+}
+
+float load_test_data_f32(void* ptr, size_t lane) { return ((float*)ptr)[lane]; }
+
+int equals(void* ptr1, void* ptr2, size_t size) {
+	for (size_t i = 0; i < size; ++i) {
+		if (((uint8_t*)ptr1)[i] != ((uint8_t*)ptr2)[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+#if defined(RANDOM_TEST_DATA)
+void fill_test_data_128(void* ptr) {
+	uint8_t* buff = (uint8_t*)ptr;
+	buff[ 0] = rand() % (UINT8_MAX + 1);
+	buff[ 1] = rand() % (UINT8_MAX + 1);
+	buff[ 2] = rand() % (UINT8_MAX + 1);
+	buff[ 3] = rand() % (UINT8_MAX + 1);
+	buff[ 4] = rand() % (UINT8_MAX + 1);
+	buff[ 5] = rand() % (UINT8_MAX + 1);
+	buff[ 6] = rand() % (UINT8_MAX + 1);
+	buff[ 7] = rand() % (UINT8_MAX + 1);
+	buff[ 8] = rand() % (UINT8_MAX + 1);
+	buff[ 9] = rand() % (UINT8_MAX + 1);
+	buff[10] = rand() % (UINT8_MAX + 1);
+	buff[11] = rand() % (UINT8_MAX + 1);
+	buff[12] = rand() % (UINT8_MAX + 1);
+	buff[13] = rand() % (UINT8_MAX + 1);
+	buff[14] = rand() % (UINT8_MAX + 1);
+	buff[15] = rand() % (UINT8_MAX + 1);
+}
+void fill_test_data_1_f32x4(float* addr) {
+	addr[0] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[1] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[2] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[3] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+}
+void fill_test_data_2_f32x4(float* addr) {
+	addr[0] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[1] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[2] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+	addr[3] = (rand() & 1 ? -1.f : 1.f) * (rand() / ((float)rand() + 1.f));
+}
+
+#else
+void fill_test_data_128(void* ptr) {
+	uint8_t* buff = (uint8_t*)ptr;
+	buff[ 0] = 0x00; buff[ 1] = 0x01; buff[ 2] = 0x02; buff[ 3] = 0x03;
+	buff[ 4] = 0x04; buff[ 5] = 0x05; buff[ 6] = 0x06; buff[ 7] = 0x07;
+	buff[ 8] = 0xFF; buff[ 9] = 0xFE; buff[10] = 0xFD; buff[11] = 0xFC;
+	buff[12] = 0xFB; buff[13] = 0xFA; buff[14] = 0xF9; buff[15] = 0xF8;
+}
+void fill_test_data_1_f32x4(float* addr) {
+	addr[0] =  13019.f / 60520.f; addr[1] =   9497.f / 49875.f;
+	addr[2] = -20204.f / 37479.f; addr[3] = -15512.f / 34346.f;
+}
+void fill_test_data_2_f32x4(float* addr) {
+	addr[0] =  5336.f / 10925.f; addr[1] = -40659.f / 28889.f;
+	addr[2] = 19322.f / 34130.f; addr[3] = -18976.f / 54979.f;
+}
+#endif
+
--- test/sse/src/test_sse.c
+++ test/sse/src/test_sse.c
@@ -1,39 +1,17 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
+#include <time.h>
+
+#define RANDOM_TEST_DATA
+
 #include "test_helpers.h"
 
 
 #define CVEC128
 #include "cvec.h"
 
-void set_test_data(void* ptr) {
-	uint8_t *buff = (uint8_t *)ptr;
-	buff[ 0] = 0x00; buff[ 1] = 0x01; buff[ 2] = 0x02; buff[ 3] = 0x03;
-    buff[ 4] = 0x04; buff[ 5] = 0x05; buff[ 6] = 0x06; buff[ 7] = 0x07;
-    buff[ 8] = 0xFF; buff[ 9] = 0xFE; buff[10] = 0xFD; buff[11] = 0xFC;
-    buff[12] = 0xFB; buff[13] = 0xFA; buff[14] = 0xF9; buff[15] = 0xF8;
-}
-void set_test_data1_vf32(void* ptr) {
-	float *buff = (float *)ptr;
-	buff[0] = 13019.f / 60520.f; buff[1] =  9497.f / 49875.f;
-	buff[2] = 20204.f / 37479.f; buff[3] = 15512.f / 34346.f;
-}
-void set_test_data2_vf32(void* ptr) {
-	float *buff = (float *)ptr;
-	buff[0] =  5336.f / 10925.f; buff[1] = 40659.f / 28889.f;
-	buff[2] = 19322.f / 34130.f; buff[3] = 18976.f / 54979.f;
-}
-
-float test_data_f32(void* ptr, size_t lane) { return ((   float *)ptr)[lane]; }
-int equals(void* ptr1, void* ptr2, size_t size) {
-	for (size_t i = 0; i < size; ++i) {
-		if (((uint8_t*)ptr1)[i] != ((uint8_t*)ptr2)[i]) {
-			return 0;
-		}
-	}
-	return 1;
-}
 
 int test_amalloc(void) {
 	uintptr_t ptr = (uintptr_t)amalloc(sizeof(double), CVEC128_MEMORY_ALIGNMENT);
@@ -49,7 +27,7 @@ int test_afree(void) {
 int test_load_store(void) {
 	uint8_t *td;
     td = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data(td);
+	fill_test_data_128(td);
     vf32x4  tdvf32 = load_vf32x4 (td);
 	float   *buff_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
 	store_vf32x4(tdvf32, buff_f32);
@@ -61,7 +39,7 @@ int test_load_store(void) {
 int test_loadu_storeu(void) {
 	uint8_t *td, *ptr;
 	td = ptr = amalloc(2 * CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); ++td;
-	set_test_data(td);
+	fill_test_data_128(td);
     vf32x4  tdvf32 = load_vf32x4 (td);
 	float   *b_f32 = amalloc(2 * CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
 	float   *buff_f32 = (float   *)((int8_t *)b_f32 + 1);
@@ -76,9 +54,9 @@ int test_loadu_storeu(void) {
 int test_at(void) {
     uint8_t *td;
     td = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data(td);
+	fill_test_data_128(td);
     vf32x4  tdvf32 = load_vf32x4 (td);
-	if (at_vf32x4(tdvf32,  0) != test_data_f32(td,  0) || at_vf32x4(tdvf32,  1) != test_data_f32(td,  1) || at_vf32x4(tdvf32,  2) != test_data_f32(td,  2) || at_vf32x4(tdvf32,  3) != test_data_f32(td,  3)){
+	if (at_vf32x4(tdvf32,  0) != load_test_data_f32(td,  0) || at_vf32x4(tdvf32,  1) != load_test_data_f32(td,  1) || at_vf32x4(tdvf32,  2) != load_test_data_f32(td,  2) || at_vf32x4(tdvf32,  3) != load_test_data_f32(td,  3)){
         return 0;
     }
     afree(td);
@@ -107,7 +85,7 @@ int test_set1(void) {
 int test_add(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = td_s_f32[i] + td_t_f32[i];
@@ -124,7 +102,7 @@ int test_add(void) {
 int test_sub(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = td_s_f32[i] - td_t_f32[i];
@@ -141,7 +119,7 @@ int test_sub(void) {
 int test_mul(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = td_s_f32[i] * td_t_f32[i];
@@ -158,7 +136,7 @@ int test_mul(void) {
 int test_div(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = td_s_f32[i] / td_t_f32[i];
 	vf32x4 dst_vf32 = div_vf32x4(load_vf32x4(td_s_f32), load_vf32x4(td_t_f32));
@@ -174,7 +152,7 @@ int test_div(void) {
 int test_max(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = MAX(td_s_f32[i], td_t_f32[i]);
@@ -190,7 +168,7 @@ int test_max(void) {
 int test_min(void) {
 	float  *td_s_f32, *td_t_f32, *td_d_f32;
 	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
-	set_test_data1_vf32(td_s_f32); set_test_data2_vf32(td_t_f32);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
 
 	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
 		td_d_f32[i] = MIN(td_s_f32[i], td_t_f32[i]);
@@ -203,8 +181,158 @@ int test_min(void) {
 	return 1;
 }
 
+int test_arcp(void) {
+	float* td_s_f32, * td_d_f32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		td_s_f32[i] = i + 2.f;
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		td_d_f32[i] = 1.f / td_s_f32[i];
+	vf32x4 dst_vf32 = arcp_vf32x4(load_vf32x4(td_s_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+	{
+		double relative_error = (at_vf32x4(dst_vf32, i) - td_d_f32[i]) / td_d_f32[i];
+		if (relative_error < 0.0) relative_error *= -1.0;
+		if (relative_error >= 1.5 / 4096.0) return 0;
+	}
+	afree(td_s_f32);afree(td_d_f32);
+	return 1;
+}
+
+int test_sqrt(void) {
+	float* td_s_f32, * td_d_f32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		td_s_f32[i] = i + 2.f;
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		td_d_f32[i] = sqrtf(td_s_f32[i]);
+	vf32x4 dst_vf32 = sqrt_vf32x4(load_vf32x4(td_s_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+	{
+		if (at_vf32x4(dst_vf32, i) != td_d_f32[i]) return 0;
+	}
+	afree(td_s_f32); afree(td_d_f32);
+	return 1;
+}
+
+int test_or(void) {
+	float* td_s_f32, * td_t_f32, * td_d_f32;
+	uint32_t* td_s_u32, * td_t_u32, * td_d_u32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	td_s_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_s_u32[i] = read_f32_as_u32(td_s_f32[i]);
+		td_t_u32[i] = read_f32_as_u32(td_t_f32[i]);
+	}
+
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_d_u32[i] = td_s_u32[i] | td_t_u32[i];
+		td_d_f32[i] = read_u32_as_f32(td_d_u32[i]);
+	}
+	vf32x4 dst_vf32 = or_vf32x4(load_vf32x4(td_s_f32), load_vf32x4(td_t_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		if (read_f32_as_u32(at_vf32x4(dst_vf32, i)) != td_d_u32[i])
+			return 0;
+	afree(td_s_f32); afree(td_t_f32); afree(td_d_f32);
+	afree(td_s_u32); afree(td_t_u32); afree(td_d_u32);
+	return 1;
+}
+
+int test_and(void) {
+	float* td_s_f32, * td_t_f32, * td_d_f32;
+	uint32_t* td_s_u32, * td_t_u32, * td_d_u32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	td_s_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_s_u32[i] = read_f32_as_u32(td_s_f32[i]);
+		td_t_u32[i] = read_f32_as_u32(td_t_f32[i]);
+	}
+
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_d_u32[i] = td_s_u32[i] & td_t_u32[i];
+		td_d_f32[i] = read_u32_as_f32(td_d_u32[i]);
+	}
+	vf32x4 dst_vf32 = and_vf32x4(load_vf32x4(td_s_f32), load_vf32x4(td_t_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		if (read_f32_as_u32(at_vf32x4(dst_vf32, i)) != td_d_u32[i])
+			return 0;
+	afree(td_s_f32); afree(td_t_f32); afree(td_d_f32);
+	afree(td_s_u32); afree(td_t_u32); afree(td_d_u32);
+	return 1;
+}
+
+int test_not(void) {
+	float* td_s_f32, * td_d_f32;
+	uint32_t* td_s_u32, * td_d_u32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	td_s_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	fill_test_data_1_f32x4(td_s_f32);
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_s_u32[i] = read_f32_as_u32(td_s_f32[i]);
+	}
+
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_d_u32[i] = ~td_s_u32[i];
+		td_d_f32[i] = read_u32_as_f32(td_d_u32[i]);
+	}
+	vf32x4 dst_vf32 = not_vf32x4(load_vf32x4(td_s_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		if (read_f32_as_u32(at_vf32x4(dst_vf32, i)) != td_d_u32[i])
+			return 0;
+	afree(td_s_f32); afree(td_d_f32);
+	afree(td_s_u32); afree(td_d_u32);
+	return 1;
+}
+
+int test_xor(void) {
+	float* td_s_f32, * td_t_f32, * td_d_f32;
+	uint32_t* td_s_u32, * td_t_u32, * td_d_u32;
+	td_s_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_f32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	td_s_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_t_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT); td_d_u32 = amalloc(CVEC128_VECTOR_SIZE, CVEC128_MEMORY_ALIGNMENT);
+	fill_test_data_1_f32x4(td_s_f32); fill_test_data_2_f32x4(td_t_f32);
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_s_u32[i] = read_f32_as_u32(td_s_f32[i]);
+		td_t_u32[i] = read_f32_as_u32(td_t_f32[i]);
+	}
+
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i) {
+		td_d_u32[i] = td_s_u32[i] ^ td_t_u32[i];
+		td_d_f32[i] = read_u32_as_f32(td_d_u32[i]);
+	}
+	vf32x4 dst_vf32 = xor_vf32x4(load_vf32x4(td_s_f32), load_vf32x4(td_t_f32));
+
+	for (size_t i = 0; i < VF32X4_NUM_ELEMENT; ++i)
+		if (read_f32_as_u32(at_vf32x4(dst_vf32, i)) != td_d_u32[i])
+			return 0;
+	afree(td_s_f32); afree(td_t_f32); afree(td_d_f32);
+	afree(td_s_u32); afree(td_t_u32); afree(td_d_u32);
+	return 1;
+}
+
 int main(int argc, char* argv[]){
 	UNUSED(argc); UNUSED(argv);
+#if defined(RANDOM_TEST_DATA)
+	unsigned seed = (unsigned)time(NULL);
+	srand(seed);
+	printf("seed:%u\n",seed);
+#endif
 	TEST_CASE(amalloc);
 	TEST_CASE(afree);
 	TEST_CASE(load_store);
@@ -218,5 +346,11 @@ int main(int argc, char* argv[]){
 	TEST_CASE(div);
 	TEST_CASE(max);
 	TEST_CASE(min);
+	TEST_CASE(arcp);
+	TEST_CASE(sqrt);
+	TEST_CASE(or);
+	TEST_CASE(and);
+	TEST_CASE(not);
+	TEST_CASE(xor);
     return 0;
 }
